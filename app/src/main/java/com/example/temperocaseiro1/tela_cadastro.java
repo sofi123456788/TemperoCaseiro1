b1fc4488e0dc2b82e44159d1fba82b4822ba01e3
cliente <ifmgsofiasantos@gmail.com>
2026-07-03 12:55:37 -0300

    1) fluxo de telas de login -> cadastro 2) voltar da tela de cadastro para login

--- app/src/main/java/com/example/temperocaseiro1/tela_cadastro.java
+++ app/src/main/java/com/example/temperocaseiro1/tela_cadastro.java
@@ -1,7 +1,6 @@
 package com.example.temperocaseiro1;
-
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,12 +8,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class tela_cadastro extends AppCompatActivity {
+    private TextView btnVoltar;
+    private TextView txtEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_cadastro);
+        setContentView(R.layout.activity_tela_cadastro); // mostra o visual da tela de cadastro
+
+        btnVoltar = findViewById (R.id.btnVoltar);
+        txtEntrar = findViewById (R.id.txtEntrar);
+
+        btnVoltar.setOnClickListener(v -> {
+            finish(); // fecha a tela atual e volta para a anterior
+        });
+
+        txtEntrar.setOnClickListener(v -> {
+            finish();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/temperocaseiro1/tela_login.java
+++ app/src/main/java/com/example/temperocaseiro1/tela_login.java
@@ -1,20 +1,32 @@
 package com.example.temperocaseiro1;
 
 import android.os.Bundle;
-
+import android.widget.TextView; // permite trabalhar com o encaminhamento de criar conta -> login
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class tela_login extends AppCompatActivity {
+
+    private TextView txtCriarConta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.tela_login);
+        setContentView(R.layout.tela_login); // mostra a tela de login (o visual dela)
+
+        txtCriarConta = findViewById(R.id.txtCriarConta); // Ela procura no XML o elemento com este ID associado à variável txtCriarConta
+
+        txtCriarConta.setOnClickListener(v -> { // quando o usuário clicar em "criar conta" executa o código abaixo
+            Intent intent = new Intent(tela_login.this, tela_cadastro.class); // "estou na tela de login e quero ir para a de cadastro"
+            startActivity(intent); // realmente abre a tela de cadastro
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
