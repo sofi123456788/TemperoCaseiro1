33a2b9d891df56302fa4427037c22ede4a313de7
anacec <anaceciliafgundesif@gmail.com>
2026-07-08 16:34:28 -0300

    todas as xml´s já foram criadas e agora só falta conctá-las para abrir a pagina quando clicar no botão.

--- app/src/main/java/com/example/temperocaseiro1/MainActivity.java
+++ app/src/main/java/com/example/temperocaseiro1/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.temperocaseiro1;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageView imgBolo = findViewById(R.id.imgBoloCenoura);
+        ImageView imgBrigadeiro = findViewById(R.id.imgBrigadeiro);
+        ImageView imgPaoDeQueijo = findViewById(R.id.imgPaoDeQueijo);
+        imgBolo.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ReceitaBoloCenouraActivity.class);
+            startActivity(intent);
+        });
+
+        imgBrigadeiro.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ReceitaBrigadeiroActivity.class);
+            startActivity(intent);
+        });
+
+        imgPaoDeQueijo.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ReceitaPaoDeQueijoActivity.class);
+            startActivity(intent);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
