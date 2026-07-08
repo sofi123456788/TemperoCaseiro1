d11f78c68566a798ed9bec65d9387e37870f54fa
anacec <anaceciliafgundesif@gmail.com>
2026-07-08 13:49:41 -0300

    receitas de bolo, brigadeiro e pão de queijo adcionadas

---
+++ app/src/main/java/com/example/temperocaseiro1/ReceitaBrigadeiroActivity.java
@@ -0,0 +1,16 @@
+package com.example.temperocaseiro1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReceitaBrigadeiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receita_brigadeiro);
+
+        setTitle("Brigadeiro");
+    }
+}
