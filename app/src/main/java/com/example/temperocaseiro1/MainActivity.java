d82feea82cf0a304689efa3ae172b1cc9b6d2b64
anacec <anaceciliafgundesif@gmail.com>
2026-07-26 18:09:29 -0300

    oi

--- app/src/main/java/com/example/temperocaseiro1/MainActivity.java
+++ app/src/main/java/com/example/temperocaseiro1/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ReceitaPaoDeQueijoActivity.class);
             startActivity(intent);
         });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
