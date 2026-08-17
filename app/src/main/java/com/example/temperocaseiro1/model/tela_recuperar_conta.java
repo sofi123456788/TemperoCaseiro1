ca69b818f4750eebc39b87310166ae54b8294996
cliente <ifmgsofiasantos@gmail.com>
2026-08-17 14:43:55 -0300

    conexão com o campo de email recuperação de conta + pré-verificação

--- app/src/main/java/com/example/temperocaseiro1/model/tela_recuperar_conta.java
+++ app/src/main/java/com/example/temperocaseiro1/model/tela_recuperar_conta.java
@@ -1,6 +1,8 @@
 package com.example.temperocaseiro1.model;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,26 @@ import com.example.temperocaseiro1.R;
 
 public class tela_recuperar_conta extends AppCompatActivity {
 
+    private EditText editEmailRecuperacao;
+    private TextView btnConfirmarEmailRecuperacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_recuperar_conta);
+
+        editEmailRecuperacao = findViewById(R.id.editEmailRecuperacao);
+
+        btnConfirmarEmailRecuperacao.setOnClickListener (v -> {
+        String emailRec = editEmailRecuperacao.getText().toString();
+
+        if(emailRec.trim().isEmpty()) {
+            editEmailRecuperacao.setError("Preencha o email de recuperação");
+        }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
